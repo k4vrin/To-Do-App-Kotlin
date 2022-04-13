4e1d45ee4cc4c4a778b42abb745d003f7093acbd
Mostafa Hosseini <kavrindev@gmail.com>
2022-04-13 14:02:33 +0430

    add add_fragment_menu.xml, update_fragment_menu.xml & fragments transition animation

--- app/src/main/java/com/kavrin/to_doapp/fragments/add/AddFragment.kt
+++ app/src/main/java/com/kavrin/to_doapp/fragments/add/AddFragment.kt
@@ -1,10 +1,8 @@
 package com.kavrin.to_doapp.fragments.add
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.kavrin.to_doapp.R
 
 class AddFragment : Fragment() {
@@ -14,6 +12,14 @@ class AddFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        val view =  inflater.inflate(R.layout.fragment_add, container, false)
+
+        setHasOptionsMenu(true)
+
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu, menu)
     }
 }
